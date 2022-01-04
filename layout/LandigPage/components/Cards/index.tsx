f7395631e6722ed454a0c0ad3910128cb0fc3eb9
Guilherme J. Floyd <guilherme.floy@alsofer.com>
2022-01-04 16:44:15 -0300

    SOME ADJUSTS

--- layout/LandigPage/components/Cards/index.tsx
+++ layout/LandigPage/components/Cards/index.tsx
@@ -29,6 +29,27 @@ export function Cards() {
     console.log(isXl)
   }, [isXl])
 
+  const cards = [
+    {
+      imageUrl: 'images/create-campaign.jpg',
+      title: 'CRIAR CAMPANHA',
+      description:
+        'Você que deseja criar uma campanha social este é o lugar certo, clique aqui...'
+    },
+    {
+      imageUrl: 'images/collab.jpg',
+      title: 'COLABORAR',
+      description:
+        'Quer ser um colaborador de uma campanha social, clique aqui...'
+    },
+    {
+      imageUrl: 'images/beneficiaries.jpg',
+      title: 'SER BENEFICIÁRIO',
+      description:
+        'Esta necessitando de auxilio, você veio ao lugar certo, clique aqui...'
+    }
+  ]
+
   return (
     <Container maxW={isXl ? '75%' : '100%'}>
       <Flex w="100%" justify="center" py="16">
@@ -37,128 +58,45 @@ export function Cards() {
           h={isXl ? '450px' : 'auto'}
           direction={isXl ? 'row' : 'column'}
         >
-          <Flex
-            direction="column"
-            w={isXl ? '300px' : '100%'}
-            borderTopRightRadius="4"
-            borderTopLeftRadius="4"
-            borderBottomRightRadius="4"
-            borderBottomLeftRadius="4"
-            bg="#F0F0F0"
-            _hover={{ boxShadow: '-1px 1px 15px 3px rgba(94,94,94,0.9);' }}
-            boxShadow="-1px 1px 5px 3px rgba(94,94,94,0.5);"
-            transition="all ease-in 0.2s"
-          >
-            <Image
-              boxSize="100%"
-              h={isXl ? '200px' : '400px'}
-              src="images/create-campaign.jpg"
+          {cards.map((card) => (
+            <Flex
+              direction="column"
+              w={isXl ? '300px' : '100%'}
               borderTopRightRadius="4"
               borderTopLeftRadius="4"
-            />
-            <Flex
-              w="100%"
-              justify="center"
-              direction="column"
-              py="2"
-              px="6"
-              _hover={{ py: '8' }}
+              borderBottomRightRadius="4"
+              borderBottomLeftRadius="4"
+              bg="#F0F0F0"
+              _hover={{ boxShadow: '-1px 1px 15px 3px rgba(94,94,94,0.9);' }}
+              boxShadow="-1px 1px 5px 3px rgba(94,94,94,0.5);"
               transition="all ease-in 0.2s"
             >
-              <Box textAlign="center" mt="4">
-                <Heading size="md" color="#E76F51">
-                  CRIAR CAMPANHA
-                </Heading>
-              </Box>
-              <Box textAlign="center" py="8">
-                <Text color="gray.600">
-                  Você que deseja criar uma campanha social este é o lugar
-                  certo, clique aqui...
-                </Text>
-              </Box>
+              <Image
+                h={isXl ? '200px' : '400px'}
+                src={card.imageUrl}
+                borderTopRightRadius="4"
+                borderTopLeftRadius="4"
+              />
+              <Flex
+                w="100%"
+                justify="center"
+                direction="column"
+                py="2"
+                px="6"
+                _hover={{ py: '8' }}
+                transition="all ease-in 0.2s"
+              >
+                <Box textAlign="center" mt="4">
+                  <Heading size="md" color="#E76F51">
+                    {card.title}
+                  </Heading>
+                </Box>
+                <Box textAlign="center" py="8">
+                  <Text color="gray.600">{card.description}</Text>
+                </Box>
+              </Flex>
             </Flex>
-          </Flex>
-          <Flex
-            direction="column"
-            w={isXl ? '300px' : '100%'}
-            borderTopRightRadius="4"
-            borderTopLeftRadius="4"
-            borderBottomRightRadius="4"
-            borderBottomLeftRadius="4"
-            bg="#F0F0F0"
-            _hover={{ boxShadow: '-1px 1px 15px 3px rgba(94,94,94,0.9);' }}
-            boxShadow="-1px 1px 5px 3px rgba(94,94,94,0.5);"
-            transition="all ease-in 0.2s"
-          >
-            <Image
-              boxSize="100%"
-              h={isXl ? '200px' : '400px'}
-              src="images/collab.jpg"
-              borderTopRightRadius="4"
-              borderTopLeftRadius="4"
-            />
-            <Flex
-              w="100%"
-              justify="center"
-              direction="column"
-              py="2"
-              px="6"
-              _hover={{ py: '8' }}
-              transition="all ease-in 0.2s"
-            >
-              <Box textAlign="center">
-                <Heading size="md" mt="4" color="#E76F51">
-                  COLABORAR
-                </Heading>
-              </Box>
-              <Box textAlign="center" py="8">
-                <Text color="gray.600">
-                  Quer ser um colaborador de uma campanha social, clique aqui...
-                </Text>
-              </Box>
-            </Flex>
-          </Flex>
-          <Flex
-            direction="column"
-            w={isXl ? '300px' : '100%'}
-            borderTopRightRadius="4"
-            borderTopLeftRadius="4"
-            borderBottomRightRadius="4"
-            borderBottomLeftRadius="4"
-            bg="#F0F0F0"
-            _hover={{ boxShadow: '-1px 1px 15px 3px rgba(94,94,94,0.9);' }}
-            boxShadow="-1px 1px 5px 3px rgba(94,94,94,0.5);"
-            transition="all ease-in 0.2s"
-          >
-            <Image
-              boxSize="100%"
-              h={isXl ? '200px' : '400px'}
-              src="images/beneficiaries.jpg"
-              borderTopRightRadius="4"
-              borderTopLeftRadius="4"
-            />
-            <Flex
-              w="100%"
-              justify="center"
-              direction="column"
-              py="2"
-              px="6"
-              _hover={{ py: '8' }}
-              transition="all ease-in 0.2s"
-            >
-              <Box textAlign="center">
-                <Heading size="md" mt="4" color="#E76F51">
-                  SER BENEFICIÁRIO
-                </Heading>
-              </Box>
-              <Box textAlign="center" py="8">
-                <Text color="gray.600">
-                  Esta necessitando de auxilio, você veio ao lugar certo, clique
-                  aqui...
-                </Text>
-              </Box>
-            </Flex>
-          </Flex>
+          ))}
         </Stack>
       </Flex>
     </Container>
